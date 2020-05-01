55d3c36a39bb73f4658f82bc3f963275c2735e51
unknown <damianmann12@gmail.com>
2020-05-01 18:35:52 +1000

    Initial

---
+++ ping.py
@@ -0,0 +1,14 @@
+import sys
+import time
+
+try:
+    from ping3 import ping, verbose_ping
+except:
+    print("Please install dependencies: ping3")
+    exit()
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
